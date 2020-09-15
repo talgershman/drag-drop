9fac36f0b0cdf5a3de942c732e171aa00e270635
gershman <tal.gershman@ironsrc.com>
2020-09-15 21:18:14 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, CdkDragStart} from '@angular/cdk/drag-drop';
+import { without } from 'lodash-es';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'metrial-ui';
+  title = 'dropdown';
+
+  private selections = [];
+  isDragging = false;
+
+  items = [
+    {
+      name: 'Moshe Moseh',
+      checked: false
+    },
+    {
+      name: 'Tal Tal',
+      checked: false
+    },
+    {
+      name: 'Avi Avi',
+      checked: false
+    },
+    {
+      name: 'Dani Dani',
+      checked: false
+    },
+    {
+      name: 'Rotem Rotem',
+      checked: false
+    },
+    {
+      name: 'Or Or',
+      checked: false
+    }
+  ];
+
+  drop(event: CdkDragDrop<string[]>): void {
+    if (this.selections.length > 1) {
+      this.items = without(this.items, ...this.selections);
+      this.items.splice(event.currentIndex - 1, 0, ...this.selections);
+    } else {
+      moveItemInArray(this.items, event.previousIndex, event.currentIndex);
+    }
+    this.isDragging = false;
+  }
+
+  dragStatred(event: CdkDragStart): void{
+    this.isDragging = true;
+  }
+
+  checkBoxChange(item: any , $event: any): void {
+    item.checked = $event.checked;
+    // tslint:disable-next-line: no-shadowed-variable
+    this.selections = this.items.filter((item: any) => item.checked);
+  }
+
+  isItemDisabled(item: any): boolean{
+    if (!this.selections.length){
+      return false;
+    }
+    return !item.checked;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
